77c15a7b41e6ab695f662ccf8e4b92464e30ed18
unknown <richard@richardhartnell.com>
2023-05-18 06:55:33 -0700

    First submission push

---
+++ src/com/generation/model/Student.java
@@ -0,0 +1,53 @@
+package com.generation.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Student
+    extends Person
+    implements Evaluation
+{
+    private double average;
+
+    private final List<Course> courses = new ArrayList<>();
+
+    private final Map<String, Course> approvedCourses = new HashMap<>();
+
+    public Student( String id, String name, String email, Date birthDate )
+    {
+        super( id, name, email, birthDate );
+    }
+
+    public void enrollToCourse( Course course )
+    {
+        //TODO implement this method
+        // this already works though? how are we implementing a function that's already functioning?
+    }
+
+    public void registerApprovedCourse( Course course )
+    {
+        approvedCourses.put( course.getCode(), course );
+    }
+
+
+    public boolean isAttendingCourse( String courseCode )
+    {
+        //TODO implement this method
+        return false;
+    }
+
+    @Override
+    public double getAverage()
+    {
+        return average;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Student {" + super.toString() + "}";
+    }
+}
